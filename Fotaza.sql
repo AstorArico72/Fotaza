b091282ba3d517f7ce5e02f67d1b65ed5e14b0f2
Astor Aricó <astorarico72@gmail.com>
2024-02-14 10:58:30 -0300

    Actualizada base de datos

--- Fotaza.sql
+++ Fotaza.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 21-12-2023 a las 00:26:10
+-- Tiempo de generación: 14-02-2024 a las 14:57:44
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.10
 
